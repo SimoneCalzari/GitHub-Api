fbcabb19b61471dd79d8d8b6dfbd420873b8370c
SimoneCalzari <calzarisimone@gmail.com>
2024-04-17 16:34:59 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// input testo
+const input = document.getElementById("input-text");
+// select
+const select = document.getElementById("select-type");
+// button ricerca
+const button = document.getElementById("button-search");
+// console.log({ input, select, button });
