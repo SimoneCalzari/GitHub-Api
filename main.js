99b48f738f125b8ac662f157a618a024a5707036
SimoneCalzari <calzarisimone@gmail.com>
2024-04-17 17:00:34 +0200

    milestone 1

--- main.js
+++ main.js
@@ -7,3 +7,26 @@ const select = document.getElementById("select-type");
 // button ricerca
 const button = document.getElementById("button-search");
 // console.log({ input, select, button });
+
+// url base api github
+const baseUrl = "https://api.github.com";
+
+button.addEventListener("click", function () {
+  axios
+    .get(`${baseUrl}/search/${select.value}`, {
+      params: {
+        q: input.value,
+      },
+      headers: config,
+    })
+    .then((response) => {
+      const results = response.data.items;
+      const ul = document.getElementById("list");
+      ul.innerHTML = "";
+      results.forEach((element) => {
+        const li = document.createElement("li");
+        li.innerText = element.name;
+        ul.append(li);
+      });
+    });
+});
